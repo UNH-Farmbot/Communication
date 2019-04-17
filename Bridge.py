55dbba5db870452e0491a8dff9079062088b8e08
tallbf <43753704+tallbf@users.noreply.github.com>
2019-04-17 05:25:10 -0400

    Update Bridge.py

--- Bridge.py
+++ Bridge.py
@@ -34,6 +34,28 @@ import time
 import serial
 from time import sleep
 
+def farmware_api_url():
+    major_version = int(os.getenv('FARMBOT_OS_VERSION', '0.0.0')[0])
+    base_url = os.environ['FARMWARE_URL']
+    return base_url + 'api/v1/' if major_version > 5 else base_url
+
+def log(message, message_type):
+    'Send a message to the log.'
+    try:
+        os.environ['FARMWARE_URL']
+    except KeyError:
+        print(message)
+    else:
+        log_message = '[UNH-FarmBot] ' + str(message)
+        headers = {
+            'Authorization': 'bearer {}'.format(os.environ['FARMWARE_TOKEN']),
+            'content-type': "application/json"}
+        payload = json.dumps(
+            {"kind": "send_message",
+             "args": {"message": log_message, "message_type": message_type}})
+        requests.post(farmware_api_url() + 'celery_script',
+                      data=payload, headers=headers)
+
 def printheader():
     print("")
     print("")
