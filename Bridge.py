d687dabbd4e36e3535aa6d4e88f5d5ddb28ae62a
tallbf <43753704+tallbf@users.noreply.github.com>
2019-04-18 09:31:09 -0400

    Update Bridge.py

--- Bridge.py
+++ Bridge.py
@@ -35,10 +35,10 @@ import serial
 import requests
 from time import sleep
 
-def farmware_api_url():
-    major_version = int(os.getenv('FARMBOT_OS_VERSION', '0.0.0')[0])
-    base_url = os.environ['FARMWARE_URL']
-    return base_url + 'api/v1/' if major_version > 5 else base_url
+#def farmware_api_url():
+#    major_version = int(os.getenv('FARMBOT_OS_VERSION', '0.0.0')[0])
+#    base_url = os.environ['FARMWARE_URL']
+#    return base_url + 'api/v1/' if major_version > 5 else base_url
 
 def log(message, message_type):
     'Send a message to the log.'
@@ -68,26 +68,6 @@ except serial.serialutil.SerialException:
 port.write(str.encode("Go"))
 sleep(0.1)
 
-# log the sending of the command
-def log(message, message_type):
-    'Send a message to the log.'
-    try:
-        os.environ['FARMWARE_URL']
-    except KeyError:
-        print(message)
-    else:
-        log_message = '[UNH-FarmBot] ' + str(message)
-        headers = {
-            'Authorization': 'bearer {}'.format(os.environ['FARMWARE_TOKEN']),
-            'content-type': "application/json"}
-        payload = json.dumps(
-            {"kind": "send_message",
-             "args": {"message": log_message, "message_type": message_type}})
-        requests.post(farmware_api_url() + 'celery_script',
-                      data=payload, headers=headers)
-
-
-
 while True:
     try:
         my_text= port.read() 
@@ -100,24 +80,6 @@ while True:
 #        new_file.write(data_output)
 #        print(data_output)
 #        new_file.close
-
-# Send to log the characteristics of plants
-def log(message, message_type):
-    'Send a message to the log.'
-    try:
-        os.environ['FARMWARE_URL']
-    except KeyError:
-        print(message)
-    else:
-        log_message = '[UNH-FarmBot] ' + str(message)
-        headers = {
-            'Authorization': 'bearer {}'.format(os.environ['FARMWARE_TOKEN']),
-            'content-type': "application/json"}
-        payload = json.dumps(
-            {"kind": "send_message",
-             "args": {"message": log_message, "message_type": message_type}})
-        requests.post(farmware_api_url() + 'celery_script',
-                      data=payload, headers=headers)
     
     except Exception as e:
         print(str(e))
