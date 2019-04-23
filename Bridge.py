c2d094aa4510ace760cdba6f9f00e69a61b434ac
tallbf <43753704+tallbf@users.noreply.github.com>
2019-04-23 11:17:04 -0400

    Update Bridge.py

--- Bridge.py
+++ Bridge.py
@@ -36,7 +36,7 @@ import requests
 from time import sleep
 from farmware_tools import device
 
-device.log('Plant Characteristiciss {{x}}, {{y}}, {{z}}.', 'success', ['toast'])  
+device.log(message='Plant Charact.:', message_type='success', channels=['toast']) 
 
 #def log(message, message_type): #'Send a send_message command to post a log to the Web App.'
  
